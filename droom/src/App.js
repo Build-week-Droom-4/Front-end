0157a362d701f41bb04ee7ddf7211b79b450dfaf
Faisal Aziz <faziz6006@gmail.com>
2020-02-03 20:02:47 -0600

    two pages

--- droom/src/App.js
+++ droom/src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import CompanyFrom from './components/CompanyForm.js';
 import './App.css';
+import JobOffers from './components/JobOffers.js';
+// import OfferList from './components/OfferList.js';
+
+
+
 
 function App() {
   return (
     <div className="App">
       <CompanyFrom/>
-        
+      <JobOffers/>
+      {/* <OfferList/> */}
     </div>
   );
 }
--- droom/src/components/JobOffers.js
+++ droom/src/components/JobOffers.js
@@ -1,3 +1,59 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
+
+const JobOffers = (props) => {
+
+  
+
+    const [data, setData] = useState();
+   
+    useEffect(() => {
+      // const id = 1;
+  
+         axios
+          .get(`https://swapi.co/api/people`)
+          .then(response => {
+            setData(response.data.results);
+            // console.log("this is job offers response", response )
+          })
+          .catch(error => {
+            console.error(error);
+          });
+  
+    },[]);
+    
+
+  
+    if (!data) {
+      return <div>Loading Your Request...</div>;
+    }
+  console.log(data);
+    // const {  stars } = movie;
+    return (
+      <div className="save-wrapper">
+        {data.map (movie => (
+          <div className="movie-card">
+          <h2>{movie.name}</h2>
+          <div className="movie-director">
+            Director: <em>{movie.gender}</em>
+          </div>
+          <div className="movie-metascore">
+            Metascore: <strong>{movie.height}</strong>
+          </div>
+          <h3>Actors</h3>
+  
+          )}
+        
+
+
+          
+        
+        <div className="save-button">Save</div>
+      </div>
+        )
+         )} </div>
+        )  
+}
+  export default JobOffers;
+  
